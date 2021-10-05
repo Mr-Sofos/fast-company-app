c9ddb9f3787c4084eafcc9d799cfc0a902cf0bd5
Muhammad <iccharho@MacBook-Air-Muhammad.local>
2021-10-05 15:41:18 +0300

    fix bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,20 @@
 import React from "react"
+import Navigation from "./NavBar"
+import { Switch, Route } from "react-router-dom"
+import Main from "./Main"
 import Users from "./Users"
 
 const App = () => {
-  return <Users />
+  return (
+    <>
+      <Navigation />
+
+      <Switch>
+        <Route to="/" exact component={Main} />
+        <Route to="/users" exact component={Users} />
+      </Switch>
+    </>
+  )
 }
 
 export default App
