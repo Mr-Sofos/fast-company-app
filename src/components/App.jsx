7cb23f130bbec802fcd28f9f4ef2415ff99d70a8
Mokhmad <mokhmad.tazbiev@gmail.com>
2021-10-06 11:23:36 +0300

    add component login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,16 @@ import Navigation from "./NavBar"
 import { Switch, Route } from "react-router-dom"
 import Main from "./Main"
 import Users from "./Users"
+import Login from "./Login"
 
 const App = () => {
   return (
     <>
       <Navigation />
-
       <Switch>
         <Route path="/" exact component={Main} />
         <Route path="/users" exact component={Users} />
+        <Route path="/login" exact component={Login} />
       </Switch>
     </>
   )
