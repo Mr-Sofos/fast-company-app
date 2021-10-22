826c5fb4524b0e84baf0acf5d8b952994b90fa1d
Mokhmad <mokhmad.tazbiev@gmail.com>
2021-10-22 18:09:31 +0300

    edit component UserCard

--- src/app/components/page/userPage/UserPage.jsx
+++ src/app/components/page/userPage/UserPage.jsx
@@ -1,30 +1,44 @@
 import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import api from "../../../api"
-import Qualities from "../../../components/ui/qualities"
-import { useHistory } from "react-router-dom"
+import UserCard from "../../ui/UserCard"
+import QualitiesCard from "../../ui/QualitiesCard"
+import MeetingsCard from "../../ui/MeetingsCard"
+import Comments from "../../ui/Comments"
+// import Qualities from "../../../components/ui/qualities"
+// import { useHistory } from "react-router-dom"
 
 const UserPage = ({ userId }) => {
-  const history = useHistory()
+  // const history = useHistory()
   const [user, setUser] = useState("")
 
   useEffect(() => {
     api.users.getById(userId).then((data) => setUser(data))
   }, [])
 
-  const handleClick = () => {
-    history.push(history.location.pathname + "/edit")
-  }
+  // const handleClick = () => {
+  //   history.push(history.location.pathname + "/edit")
+  // }
 
   if (user) {
     return (
-      <div>
-        <h1>{user.name}</h1>
+      <div className="container">
+        <div className="row gutters-sm">
+          <div className="col-md-4 mb-3">
+            <UserCard user={user} />
+            <QualitiesCard data={user.qualities} />
+            <MeetingsCard value={user.completedMeetings} />
+          </div>
+          <div className="col-md-8">
+            <Comments />
+          </div>
+        </div>
+        {/* <h1>{user.name}</h1>
         <h2>Профессия: {user.profession.name}</h2>
         <Qualities qualities={user.qualities} />
         <p>completedMeetings: {user.completedMeetings}</p>
         <h2>Rate: {user.rate}</h2>
-        <button onClick={handleClick}>Изменить</button>
+        <button onClick={handleClick}>Изменить</button> */}
       </div>
     )
   } else {
@@ -33,7 +47,7 @@ const UserPage = ({ userId }) => {
 }
 
 UserPage.propTypes = {
-  userId: PropTypes.string
+  userId: PropTypes.string.isRequired
 }
 
 export default UserPage
