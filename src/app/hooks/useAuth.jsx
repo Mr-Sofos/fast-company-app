9d75b10eafd05033454b752715e87b494f74c457
Mokhmad Tazbiev <mokhmad.tazbiev@gmail.com>
2021-11-30 15:37:02 +0300

    add a post request for authorization

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -1,12 +1,27 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 const AuthContext = React.createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({ children }) => {
-    return <AuthContext.Provider>{children}</AuthContext.Provider>;
+    async function signUp({ email, password }) {
+        const key = "AIzaSyCMCmNWoyiYKZGNYgETXPDWC_1o2sy-IxE";
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
+        const { data } = await axios.post(url, {
+            email,
+            password,
+            returnSecureToken: true
+        });
+        console.log(data);
+    }
+    return (
+        <AuthContext.Provider value={{ signUp }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 AuthProvider.propTypes = {
