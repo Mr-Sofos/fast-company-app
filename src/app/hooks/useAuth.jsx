0c0755cb12d0081dd07de8c8494d32407fb3d646
Mokhmad Tazbiev <mokhmad.tazbiev@gmail.com>
2021-12-01 16:39:57 +0300

    space

--- src/app/hooks/useAuth.jsx
+++ src/app/hooks/useAuth.jsx
@@ -16,12 +16,14 @@ const EXPIRES_KEY = "jwt-expires";
 const AuthProvider = ({ children }) => {
     const [currentUser, setUser] = useState({});
     const [error, setError] = useState(null);
+
     function setTokens({ refreshToken, idToken, expiresIn = 3600 }) {
         const expiresDate = new Date().getTime() + expiresIn * 1000;
         localStorage.setItem(TOKEN_KEY, idToken);
         localStorage.setItem(REFRESH_KEY, refreshToken);
         localStorage.setItem(EXPIRES_KEY, expiresDate);
     }
+
     async function signUp({ email, password, ...rest }) {
         const key = "AIzaSyCMCmNWoyiYKZGNYgETXPDWC_1o2sy-IxE";
         const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`;
@@ -38,6 +40,7 @@ const AuthProvider = ({ children }) => {
             errorCatcher(error);
         }
     }
+
     async function createUser(data) {
         try {
             const { content } = await userService.create(data);
@@ -46,16 +49,19 @@ const AuthProvider = ({ children }) => {
             errorCatcher(error);
         }
     }
+
     function errorCatcher(error) {
         const { message } = error.response.data;
         setError(message);
     }
+
     useEffect(() => {
         if (error !== null) {
             toast(error);
             setError(null);
         }
     }, [error]);
+
     return (
         <AuthContext.Provider value={{ signUp, currentUser }}>
             {children}
