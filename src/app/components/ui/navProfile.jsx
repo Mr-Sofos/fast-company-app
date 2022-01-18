9ab645ab5b254dd36b81c18324318c43d3ae2237
Mokhmad Tazbiev <mokhmad.tazbiev@gmail.com>
2022-01-18 17:51:19 +0300

    addUserInNavbar

---
+++ src/app/components/ui/navProfile.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
+
+const NavProfile = () => {
+    const [isOpen, setOpen] = useState(false);
+    const { currentUser } = useAuth();
+    const toggleMenu = () => setOpen((prevState) => !prevState);
+    return (
+        <div className="dropdown" onClick={toggleMenu}>
+            <div className="btn dropdown-toggle d-flex align-items-center">
+                <div className="me-2">{currentUser.name}</div>
+                <img
+                    src={`https://avatars.dicebear.com/api/avataaars/${(
+                        Math.random() + 1
+                    )
+                        .toString(36)
+                        .substring(7)}.svg`}
+                    className="rounded-circle shadow-1-strong me-3"
+                    alt="avatar"
+                    width="65"
+                    height="65"
+                />
+            </div>
+            <div className={`w-100 dropdown-menu ${isOpen ? "show" : ""}`}>
+                <Link
+                    className="dropdown-item"
+                    to={`/users/${currentUser._id}`}
+                >
+                    Profile
+                </Link>
+                <Link className="dropdown-item" to="logout">
+                    Log Out
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default NavProfile;
